9b4f503afa5872c40c557a21434ecec4de973aeb
Yeroshenko <valeriy.yeroshenko@gmail.com>
2019-08-02 19:06:41 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let start = document.getElementById('start'), 
+    budgetValue = document.querySelector('.budget-value'),
+    daybudgetValue = document.querySelector('.daybudget-value'),
+    levelValue = document.querySelector('.level-value'),
+    expensesValue = document.querySelector('.expenses-value'),
+    optionalexpensesValue = document.querySelector('.optionalexpenses-value'),
+    incomeValue = document.querySelector('.income-value'),
+    monthsavingsValue = document.querySelector('.monthsavings-value'),
+    yearsavingsValue = document.querySelector('.yearsavings-value'),
+    expensesItem = document.querySelectorAll('.expenses-item'),
+
+    itemsBtn = document.getElementsByTagName('button'),
+    expensesItemBtn = itemsBtn[0],
+    optionalExpensesBtn = itemsBtn[1],
+    countBudgetBtn = itemsBtn[2],
+
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    chooseIncome = document.querySelector('#income'),
+    savings = document.querySelector('#savings'),
+    chooseSum = document.querySelector('#sum'),
+    choosePercent = document.querySelector('#percent'),
+
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+
+     
